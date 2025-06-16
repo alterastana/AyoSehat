e3099904f923ed9872a5d21304f7f13c83fb4fe4
RifqyAbdillah <rifqyabdillah3@gmail.com>
2025-06-16 14:32:51 +0700

    jadwal dokter

--- jadwal_dokter.php
+++ jadwal_dokter.php
@@ -1,3 +1,20 @@
+<?php 
+
+include "service/database.php";
+session_start();
+
+$db = Database::getConnection();
+
+if (!isset($_SESSION["is_login"])) {
+    header("location: login.php");
+    exit();
+}
+$sql = "SELECT * FROM dokter";
+$stmt = $db->prepare($sql);
+$stmt->execute();
+$result = $stmt->get_result();
+$stmt->close();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,8 +24,26 @@
 </head>
 <body>
     <?php include "layout/header.html" ?>
+    <h2>Data Jadwal</h2>
+    <table border = 1>
+        <tr>
+            <th>ID</th>
+            <th>Nama</th>
+            <th>Spesialisasi</th>
+            <th>Biaya</th>
+            <th>Jadwal</th>
+        </tr>
+        <?php while($row = $result->fetch_assoc()): ?>
+        <tr>
+        <td><?= $row['id']; ?></td>
+            <td><?= $row['nama']; ?></td>
+            <td><?= $row['spesialisasi']; ?></td>
+            <td><?= $row['biaya_konsultasi']; ?></td>
+            <td><?= $row['jadwal']; ?></td>
+        </tr>
+        <?php endwhile; ?>
+    </table>
     <main>
-        Tampilan tabel dokter di sini
     </main>
     <?php include "layout/footer.html" ?>
 </body>
